7a82db13b1f2cf6569e044379a3d7dbf2ec15e4f
Mo7amedSMahdi <mo7ammed.salah@outlook.com>
2023-07-09 00:57:48 +0300

    🐛 fix(entrypoint.sh): fix path to generate_pr.py script to use the correct path

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ else
     pull_request_id="$INPUT_PULL_REQUEST_ID"
 fi
 
-python3 generate_pr.py \
+/action/generate_pr.py \
     --github-api-url "$GITHUB_API_URL" \
     --github-token "$INPUT_GITHUB_TOKEN" \
     --pull-request-id "$pull_request_id" \
